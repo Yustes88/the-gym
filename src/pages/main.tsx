f1facbd4ca1e410a5a81a3e4aee88f82fc10ee42
Yulya <iuliiaobr.io@gmail.com>
2023-04-07 11:49:43 +0700

    Add header menu component

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,8 +1,13 @@
+import { HeaderResponsive } from "@/components/Header";
 import { HeroImageBackground } from "@/components/Hero";
+import { HeaderItems } from "@/data/data";
 
 export default function Main() {
   return (
+    <>
+    <HeaderResponsive links = {HeaderItems}/>
     <HeroImageBackground/>
+    </>
   )
 }
 
