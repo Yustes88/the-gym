3785656541adc320073602fcba2e382a6f9b9e27
Yulya <iuliiaobr.io@gmail.com>
2023-04-11 10:49:40 +0700

    Add more mock news

--- src/components/interval-images/data.ts
+++ src/components/interval-images/data.ts
@@ -16,4 +16,24 @@ export const Images = [
     imgSrc: 'news/news-3.jpg',
     alt: '',
   },
+  {
+    id: randomId(),
+    imgSrc: 'news/news-4.jpg',
+    alt: '',
+  },
+  {
+    id: randomId(),
+    imgSrc: 'news/news-5.jpg',
+    alt: '',
+  },
+  {
+    id: randomId(),
+    imgSrc: 'news/news-6.jpg',
+    alt: '',
+  },
+  {
+    id: randomId(),
+    imgSrc: 'news/news-7.jpg',
+    alt: '',
+  },
 ]
